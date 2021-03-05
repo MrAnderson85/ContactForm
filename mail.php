74f4317d1317aa3cad3678a94aca7b5c072c2913
MrAnderson85 <mranderson85@gmail.com>
2021-03-05 08:57:18 -0500

    Added first page

---
+++ mail.php
@@ -0,0 +1,16 @@
+<?php
+$name = $_POST['name'];
+$email = $_POST['emailAddress'];
+
+$to = "mranderson85@gmail.com";
+
+$subject = "test Mail";
+
+$txt = "Name: ". $name . "\r\n Email: " . $email ;
+
+$headers = "From: noreply@testsite.com";
+
+if($email!=NULL)
+    mail($to,$subject,$to,$headers)
+
+?>
